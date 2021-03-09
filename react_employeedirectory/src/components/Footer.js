e1e74b461af209686d8c2c453087391faf69fc4b
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-09 16:12:22 -0500

    create footer function

---
+++ react_employeedirectory/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Taylor Ceneviva GitHub:@taylorceneviva &amp; OSU Coding Bootcamp</span>
+    </footer>
+  );
+}
+
+export default Footer;
