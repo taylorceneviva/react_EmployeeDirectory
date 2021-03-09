9c1f51457b9edf709dc30431ecd2aa481cacd017
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-09 16:10:28 -0500

    add style to table

--- react_employeedirectory/src/components/EmployeeTable.js
+++ react_employeedirectory/src/components/EmployeeTable.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 // actual table to hold data
 
 function EmployeeTable(props) {
   console.log(props);
-    return (
-        <table className= "Table striped bordered hover responsive">
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Cell</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.users.map( user => {
-            return (
-              <tr key={user.login.uuid} >
-              <td><img alt={user.name.first +  user.name.last} src={user.picture.thumbnail}/></td>
+  return (
+    <table class="col-12" style={{ margin: "auto", fontSize: "18px", textAlign: "center"}}>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Cell</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.users.map((user) => {
+          return (
+            <tr key={user.login.uuid}>
+              <td>
+                <img
+                  alt={user.name.first + user.name.last}
+                  src={user.picture.large}
+                />
+              </td>
               <td>{user.name.first}</td>
               <td>{user.name.last}</td>
               <td>{user.cell}</td>
               <td>{user.email}</td>
-            </tr> 
-            )
-
-          }
-
-            
-            )}
-        </tbody>
-      </table>
-    );
-  }
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 
 export default EmployeeTable;
