4203f17f9ebfa8fa9e2759296ccd883d5bbb8fc5
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-09 21:24:37 -0500

    try to fix sorting

--- react_employeedirectory/src/components/EmployeeData.js
+++ react_employeedirectory/src/components/EmployeeData.js
@@ -14,25 +14,39 @@ class EmployeeData extends Component {
       // console.log(results)
       this.setState({
         Employee: results.data.results,
-        results: results.data.results
+        results: results.data.results,
       });
     });
   }
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     console.log(event.target.value);
     const term = event.target.value;
-    const filteredList = this.state.Employee.filter(user => {
-        let values = Object.values(user).join("").toLowerCase();
-        return values.indexOf(term.toLowerCase()) !== -1;
-    })
-    this.setState({results: filteredList});
+    const filteredList = this.state.Employee.filter((user) => {
+      let values = Object.values(user).join("").toLowerCase();
+      return values.indexOf(term.toLowerCase()) !== -1;
+    });
+    this.setState({ results: filteredList });
   }
+
+//      sortByName = () => {
+
+//       const sorted = this.state.Employee.sort((a, b) => {
+//           if (a.name.first < b.name.first) {
+//               return -1;
+//           } else if (a.name.first > b.name.first) {
+//               return 1;
+//           } else {
+//               return 0;
+//           }
+//       });
+
+//       this.setState([...sorted]);
+//   }
+
   render() {
     return (
       <div>
-        <FindEmployee 
-        handleInputChange={this.handleInputChange}
-        />
+        <FindEmployee handleInputChange={this.handleInputChange} />
         <EmployeeTable users={this.state.results} />
       </div>
     );
